c0c1f033688cfd06b92c4b8907b8d0a5e39e8d5d
AoThen <32790381+AoThen@users.noreply.github.com>
2022-10-02 12:00:27 +0800

    界面

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # C1py
 C1 API Used By Python
+
+Example
--- main.py
+++ main.py
@@ -1,2 +1,37 @@
-if __name__ == '__main__':
+import sys
+
+from PyQt6.QtWidgets import QApplication, QMainWindow
+
+from untitled import Ui_Dialog
+
+
+def login(address='localhost', port=8080, user='admin', password='123'):
     print('Hi C1py')
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def clearText(self):
+        print(999)
+
+    def queryWeather(self):
+        print(666)
+
+    def initUI(self):
+        myWin = Ui_Dialog()
+        myWin.setupUi(self)
+
+
+def main():
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    login()
+    main()
